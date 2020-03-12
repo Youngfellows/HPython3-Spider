1cd75c47e7c51069f7dc162567245d31f27f4df6
Youngfellows <hk20130120515@163.com>
2020-03-13 03:11:50 +0800

    xxx

--- pandora/day01_spider/03_biqukan_downloader2.py
+++ pandora/day01_spider/03_biqukan_downloader2.py
@@ -2,6 +2,9 @@
 from lxml import etree
 import requests
 import re
+import collections
+import json
+import os
 
 """
 笔趣看网络小说下载器
@@ -20,11 +23,13 @@ class Downloader(object):
         self.__head = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166  Safari/535.19',
         }
+        self._novel_dict = collections.OrderedDict()  # 保存章信息的字典
+        self.novel_info = collections.OrderedDict()  # 小说信息的字典
 
-    # 获取小说每一张的名字和下载连接
-    def get_download_url(self):
+    # 网络请求
+    def __requests_get(self, target):
         # 请求响应
-        response = requests.get(url=self.__target_url, headers=self.__head, verify=False)
+        response = requests.get(url=target, headers=self.__head, verify=False)
         # 方式1: 解决中文乱码问题
         html = response.content
         # html = html.decode('utf-8', 'ignore')
@@ -35,7 +40,11 @@ class Downloader(object):
         # response.encoding = "gbk"
         # html = response.text
         print(html)
+        return html
 
+    # 获取小说详情和大纲目录
+    def get_novel_info(self):
+        html = self.__requests_get(self.__target_url)
         # 解析HTML
         dom_tree = etree.HTML(html)
 
@@ -69,16 +78,63 @@ class Downloader(object):
         print("章标题: {}".format(catalog_titles))
         print("章连接: {}".format(catalog_links))
         # 正则处理有效章节标题和连接
+        pattern = re.compile(r'[第弟](.+)[章张]', re.IGNORECASE)
+        numbers = 1  # 保存章节索引
+        for title, link in zip(catalog_titles, catalog_links):
+            print("{},{}".format(title, link))
+            # 正则匹配
+            matchObj = pattern.match(title)
+            # print("{0},matchObj = {1}".format(title, matchObj))
+            if matchObj != None:
+                names = str(title).split('章')
+                names2 = str(title).split('张')
+                index = pattern.findall(names[0] + '章')
+                if names2[0] != str(title):
+                    index = pattern.findall(names2[0] + "张")
+                    names = names2
+                    # print("1 index: {},names: {}".format(index, names))
+                # print("index: {},name: {}".format(index, names[1]))
+                key = "第" + str(numbers) + "章" + names[1]
+                self._novel_dict[key] = self.__host + link  # 添加有效章节名称+下载连接到字典
+                numbers += 1
+                print(key)
 
+        # 添加小说字典信息
+        self.novel_info["name"] = novel_name
+        self.novel_info["author"] = novel_author
+        self.novel_info["poster"] = novel_poster
+        self.novel_info["intro"] = novel_intro
+        self.novel_info["novels"] = self._novel_dict
+
+        print("**************** 小说详情 JSON字符串 ************************")
+        file_name = "./novel/" + novel_name + "_大纲.json"
+        # 删除旧文件
+        if file_name in os.listdir():
+            os.remove(file_name)
+
+        # 将字典转化为JSON字符串,禁用ascii编码格式，返回的Unicode字符串，方便使用
+        details = json.dumps(self.novel_info, ensure_ascii=False)  # 小说详情
+        print(details)
+        # 将Unicode字符串以utf-8编码写人文件
+        with open(file_name, "wb") as fp:
+            fp.write(details.encode("utf-8"))
+
+        # 返回小说详情JSON
+        return details
+
+    # 下载每一章小说
+    def download(self):
         # 3.具体内容
         # 章内容
         # 上一章
         # 下一章
-
-
-# 下载每一章小说
-def download(self):
-    pass
+        # 获取小说下载列表信息
+        for title, link in self._novel_dict.items():
+            print("\n{}: {}\n".format(title, link))
+            html = self.__requests_get(link)  # 网络请求
+            dom_tree = etree.HTML(html)  # 解析HTML
+            contents = dom_tree.xpath('//div[@class="showtxt"]')
+            print(contents)
 
 
 # 程序入口
@@ -93,4 +149,16 @@ if __name__ == "__main__":
     # 创建下载器
     dl = Downloader(target_url)
     # 获取下载列表信息
-    dl.get_download_url()
+    details_novel = dl.get_novel_info()
+    print("***************************** 获取小说字典信息  ****************************")
+    # 获取小说字典信息
+    for key, value in dl.novel_info.items():
+        print("{}:{}".format(key, value))
+
+    print("***************************** 获取小说JSON信息  ****************************")
+    # 使用JSON解析处理
+    print(details_novel)
+
+    print("***************************** 下载小说  ************************************")
+    # 下载每页小说并保存
+    dl.download()
