0f09cf1a174bcc3690587ffcbdb23da6e625934c
Youngfellows <hk20130120515@163.com>
2020-03-12 22:44:47 +0800

    xxx

--- pandora/day01_spider/01_requests_gitbook.py
+++ pandora/day01_spider/01_requests_gitbook.py
@@ -5,15 +5,19 @@ import requests
 
 if __name__ == "__main__":
     target = "https://gitbook.cn/"
-    response = requests.get(url=target)
-
-    # 响应文本
+    # 请求头
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+    }
+    # 网络请求
+    requests.packages.urllib3.disable_warnings()  # 去除警告
+    response = requests.get(url=target, headers=headers, verify=False)  # 忽略证书认证
     html = response.text
     print(html)
 
     print("*" * 40)
     # 响应的头信息
-    resopnse_head = requests.head(url=target)
+    resopnse_head = requests.head(url=target, headers=headers, verify=False)
     print("type(resopnse_head):{}".format(type(resopnse_head)))
     headers = resopnse_head.headers
     # print(headers)
